c4dce4dd64c3919a692a9deeaf59b51a11fc5a4f
Javier Jimenez <javier.sniguel@gmail.com>
2024-06-11 10:26:44 -0300

    se crearon los metodos abstractos con los ataques basicos de cada pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,8 +1,14 @@
 public abstract class Pokemon {
+
     protected int numPokedex;
     protected String nombre;
-    protected String peso;
-    protected String temporada;
+    protected double peso;
+    protected int temporada;
 
+    protected abstract void atacarPlacaje();
+
+    protected abstract void atacarMordisco();
+
+    protected abstract void atacarAranazo();
 
 }
