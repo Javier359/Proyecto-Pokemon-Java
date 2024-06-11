f4a76e9a46bea7bfbe5e9e4826d3601bb26fb46a
Javier Jimenez <javier.sniguel@gmail.com>
2024-06-11 11:37:46 -0300

    se crearon los pokemones con nombres propios y sus atributos en constructor

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ public class Main {
     public static void main(String[] args) {
 
         //creacion de objeto pokemon
-//        Pikachu pikachu = new Pikachu();
-//        Charmander charmander = new Charmander();
-//        Bulbasaur bulbasaur = new Bulbasaur();
-//        Squirtle squirtle = new Squirtle();
-//
-//        pikachu.atacarAranazo();
-//        pikachu.atacarImpactrueno();
-//        charmander.atacarAranazo();
-//        charmander.aracarLanzallamas();
-//        bulbasaur.atacarAranazo();
-//        bulbasaur.atacarDrenaje();
-//        squirtle.atacarAranazo();
-//        squirtle.atacarBurbuja();
+        Pikachu pikachu = new Pikachu("Pablo", 10, 15, 1);
+        Charmander charmander = new Charmander("Llamitas", 1, 18, 1);
+        Bulbasaur bulbasaur = new Bulbasaur("Rosa", 2, 22, 1);
+        Squirtle squirtle = new Squirtle("Tortugon", 3, 31, 1);
+
+        pikachu.atacarAranazo();
+        pikachu.atacarImpactrueno();
+        charmander.atacarAranazo();
+        charmander.aracarLanzallamas();
+        bulbasaur.atacarAranazo();
+        bulbasaur.atacarDrenaje();
+        squirtle.atacarAranazo();
+        squirtle.atacarBurbuja();
     }
 }
