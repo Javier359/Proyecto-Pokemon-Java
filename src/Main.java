66c7cb489ebfe3231592e082085087c83e6bf9ac
Javier Jimenez <javier.sniguel@gmail.com>
2024-06-11 11:04:57 -0300

    se crearon los objetos con cada pokemon

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        //creacion de objeto pokemon
+        Pikachu pikachu = new Pikachu();
+        Charmander charmander = new Charmander();
+        Bulbasaur bulbasaur = new Bulbasaur();
+        Squirtle squirtle = new Squirtle();
     }
 }
